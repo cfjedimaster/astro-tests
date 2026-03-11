3eec259ab303b04c50b40aadac9abf95205a0292
Raymond Camden <raymondcamden@gmail.com>
2026-03-11 15:51:19 -0500

    whitespace

--- tmdb-movie-browser-v6/astro.config.mjs
+++ tmdb-movie-browser-v6/astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { memoryCache } from 'astro/config';
-import node from '@astrojs/node';
 
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
     adapter: netlify(),
-experimental: {
-    cache: { provider: memoryCache() },
-  },
+    experimental: {
+        cache: { provider: memoryCache() },
+    },
 });
